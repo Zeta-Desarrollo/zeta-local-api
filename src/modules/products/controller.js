ec1ad8d8828d7b41c492dd182c11dd5a122b6fb1
Zeta Desarrollo <desarrollo@zeta.com.ve>
2026-01-07 13:23:01 -0400

    no price label

--- src/modules/products/controller.js
+++ src/modules/products/controller.js
@@ -246,6 +246,147 @@ async function JSPDF (body, params){
     }
 }
 
+async function noPriceLabel (body, params){
+    let e
+    let product = "lol"
+    global.window = {document: {createElementNS: () => {return {}} }};
+    global.navigator = {};
+    global.btoa = () => {};
+    let FS
+    // Default export is a4 paper, portrait, using millimeters for units
+    try{
+        let pdfName = "Bulk "+ (new Date()+"").replace(/:/g,"-")
+        const merger = new PDFMerger()
+        const productData = {
+
+        }
+        const result = await sql.query(PRODUCTS_BY_CODES(body.products, body.props.location, true, true, true, body.props.priceList.value))
+        if (result.recordset.length===0) throw "invalid-codes"
+        if (body.products.length==1){
+            product=result.recordset[0]
+        }
+        const allProducts = result.recordset
+        for (const product of allProducts){
+            productData[product.ItemCode] = product
+        }
+
+
+        for (const ItemCode of body.products){
+            const product = productData[ItemCode]
+            
+            const doc = new jsPDF({
+                orientation: "landscape",
+                unit: "cm",
+                format: [15.24, 10.08],
+                
+            });
+            
+            const leftEdge = 1.8
+            const leftSpace = 1
+            const rightEdge = 12.5
+            
+            doc.setFontSize(16)
+            
+            //generate qr
+            const url = `http://${process.env.FRONT_IP}/#/consulta/${product.ItemCode}`
+            const filePath = `./public/${product.ItemCode}.png`
+
+            if(!fs.existsSync(filePath)){
+                await qrcode.toFile(filePath,url, {
+                    version:4,
+                    errorCorrectionLevel:"M",
+                    color:{
+                        light: '#0000'
+                    }
+                })
+            }
+            const refWhiteFile = fs.readFileSync("./public/ref-no-ring.png")
+            const refWhite = new Uint8Array(refWhiteFile);
+            //qr side
+            const qrFile = fs.readFileSync("./public/"+product.ItemCode+".png")
+            const qr = new Uint8Array(qrFile);
+            doc.addImage(qr, "PNG", leftEdge, 1, 5, 5)
+            // doc.addImage(refWhite, "PNG", 3.6, 2.5, 1.3, 1.3)
+            //qr s
+
+            const logoFile = fs.readFileSync("./public/zeta-negro.png")
+            const logo = new Uint8Array(logoFile);
+            doc.addImage(logo, "PNG", 8 , 4.2, 4.14, 1.2)
+            
+            
+            doc.setFont("Helvetica", "bold")
+            doc.setFontSize(16)
+            doc.setFillColor("#000000")
+            // doc.setTextColor("#ffffff")
+            doc.text(product.ItemCode, rightEdge, 1, "right")
+            doc.setFontSize(16)
+
+            let marcaText = product.FirmCode != -1? product.FirmName : ''
+            // let line2 = doc.splitTextToSize(marcaText, rightEdge - leftEdge - leftSpace -4)
+            FS=14
+            doc.setFontSize(FS)
+
+            // while (line2.length> 1){
+            //     FS-=0.1
+            //     doc.setFontSize(FS)
+            //     line2 = doc.splitTextToSize(marcaText, rightEdge - leftEdge - leftSpace -4)
+            // }
+            doc.text(marcaText, leftEdge+0.5, 1, "left")
+
+
+            doc.setFontSize(16)
+            doc.setTextColor("#000000")
+            doc.setFillColor("#ffffff")
+
+            doc.setFont("Helvetica", "")
+            
+            FS =  16
+            doc.setFontSize(FS)
+            
+            
+            let line = doc.splitTextToSize(product.ItemName, rightEdge - leftEdge - leftSpace -4)
+
+            while (line.length * FS > (body.props.showPrices?50:120)){
+                FS-=0.1
+                doc.setFontSize(FS)
+                line = doc.splitTextToSize(product.ItemName, rightEdge - leftEdge - leftSpace -4)
+            }
+            doc.text(line, leftEdge+leftSpace+4, 1.8, "left")
+            doc.setFontSize(16)
+            
+
+            doc.setFillColor("#000000")
+            
+            doc.save("./docs/"+product.ItemCode+".pdf")
+            let i = 0
+            while (i<body.props.copies){
+                await merger.add("./docs/"+product.ItemCode+".pdf");
+                i++
+            }
+
+        }
+        await merger.save(`./docs/${pdfName}.pdf`)
+
+    await new Promise((resolve, reject)=>{
+        ptp.print("./docs/"+pdfName+".pdf", {
+            printer:"Etiquetas",
+            orientation:"landscape",
+            scale:"shrink",
+            
+        }).then(resolve).catch(reject);
+    })
+    delete global.window;
+    delete global.navigator;
+    delete global.btoa;
+    }catch(error){
+        console.log("what happened?", new Date(), error)
+        e = error.message? error.message :error
+    }
+    return {
+        res:product,
+        error:e
+    }
+}
 
 async function storageLabel (body, params){
     let e
@@ -572,7 +713,12 @@ const controller = {
             if (body.props.storageLabel){
                 result = await storageLabel(body, params)
             }else{
-                result = await JSPDF(body, params)
+                if(body.props.noPriceLabel){
+                    result = await noPriceLabel(body, params)
+
+                }else{
+                    result = await JSPDF(body, params)
+                }
             }
             if (result.error){
                 e = result.error
