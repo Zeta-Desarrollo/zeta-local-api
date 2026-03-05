cb1a79b40b7f0aca6cf7691ed25d63f1f24ea876
Zeta Desarrollo <desarrollo@zeta.com.ve>
2026-03-05 10:53:35 -0400

    router

--- src/modules/quotation/router.js
+++ src/modules/quotation/router.js
@@ -11,6 +11,6 @@ quotationRouter.post("/list", devFunction, isAuth, checkPermissions(["cotizacion
 quotationRouter.post("/get", devFunction, isAuth, checkPermissions(["cotizaciones"]), callController(controller.get))
 quotationRouter.post("/generate", devFunction, isAuth, checkPermissions(["cotizaciones"]), callController(controller.generate))
 
-productsRouter.get("/price-lists",devFunction, isAuth, callController(controller.getPriceLists))
-productsRouter.get("/defaultPriceList",devFunction, callController(controller.getDefaultPriceList))
+quotationRouter.get("/price-lists",devFunction, isAuth, callController(controller.getPriceLists))
+quotationRouter.get("/defaultPriceList",devFunction, callController(controller.getDefaultPriceList))
 export default quotationRouter
