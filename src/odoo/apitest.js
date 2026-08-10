3151c4282cede187670c748ae4857cbadb8afa42
Zeta Desarrollo <desarrollo@zeta.com.ve>
2026-08-10 12:30:34 -0400

    maping for ticket done

--- src/odoo/apitest.js
+++ src/odoo/apitest.js
@@ -62,24 +62,31 @@ export default async function (){
     
     await odoo.authenticate("zetaca-staging-35995145", "api.zetainterno_1@gallerycomputer.local", "12349876*")
 
-    const productIds = await odoo.execute("product.template", "search", [[["default_code", "=", "1009648"]]], {offset:0, limit:6})
+    const productIds = await odoo.execute("product.template", "search", [[["default_code", "=", "1009648"], ]], {offset:0, limit:6})
     console.log("product ids", productIds)
 
-    const data = await odoo.execute("product.template", "read", productIds, {fields:["name", "list_price", "l10n_ve_old_code", "default_code", "categ_id", "product_brand_id"]})
-
+    const data = await odoo.execute("product.template", "read", productIds, {fields:["name", "list_price","l10n_ve_is_agreement_product", "l10n_ve_old_code", "default_code", "categ_id", "product_brand_id", "supplier_taxes_id"]})
+    console.log("data", data)
     const stockids = await odoo.execute("stock.quant", "search", [
         [
             ["product_tmpl_id", "=", productIds[0]],
             ["location_id", "=", 38]
         ]
     ])
-    console.log("stock ids", stockids)
-
     const stock = await odoo.execute("stock.quant", "read", stockids, {fields:["quantity"]})
-    console.log("STOCK", stock)
-
     // const category = await odoo.execute("product.category", "read", [data[0].categ_id[0]])
     // console.log("cat", category)
+
+    const pricelist = await odoo.execute("product.pricelist", "search", [[]], {})
+    console.log("pricelist ids", pricelist, )
+
+    const saleOrderIds = await odoo.execute("sale.order", "search", [[]],{})
+    console.log("sale orders ids", saleOrderIds)
+    // const saleOrders = await odoo.execute("sale.order", "read", [[4]], {})
+
+    const taxes = await odoo.execute("account.tax", "read", [55], {})
+    console.log("taxes", taxes)
+
 
 }
 
@@ -93,7 +100,15 @@ const DATA_MAP ={
     //stock.quant (Filter for location_id = 38)
     onHand: "quantity",
 
-    //product.pricelist
-    Price: ""
+    //product.template
+    Price: "list_price",
+    FirmName: "product_brand_id", //[1] 
+    FirmCode: "product_brand_id", //[0]
+    
+    TaxCodeAR:"supplier_taxe_id", // Array vacio -> IVA_EXE / Id-> account.tax
+    FrozenFor:"sale_ok"
+
 
 }
+
+//l10n_ve_is_agreement_product always true?
